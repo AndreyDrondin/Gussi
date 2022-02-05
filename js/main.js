e6f4a043955541110e6d43929b649f5b45357b5b
Andrey Drondin <a_drondin@inbox.ru>
2022-02-05 17:32:16 +0300

    release v1

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function () {
     speed: 1000,
     fade: true,
     cssEase: "linear",
-    prevArrow: "<img src='../img/icons/arrow_left.svg' class='prev' alt='1'>",
-    nextArrow: "<img src='../img/icons/arrow_right.svg' class='next' alt='2'>",
+    prevArrow: "<img src='img/icons/arrow_left.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='img/icons/arrow_right.svg' class='next' alt='2'>",
   });
 });
